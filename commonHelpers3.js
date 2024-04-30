95ea0dc3247c2cf70e7e5371ee848e706d1dcbba
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-04-30 19:54:44 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-06.2@09173b444d10659931a36f4904e97c3553534944 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const n=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"],s=document.querySelector("#ingredients");let e=[];n.forEach(o=>{const t=document.createElement("li");t.textContent=o,t.classList="item",e.push(t)});s.append(...e);
+//# sourceMappingURL=commonHelpers3.js.map
