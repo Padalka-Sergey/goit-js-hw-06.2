428a66c6567c36d30176a16cccec8f873137964a
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-06-19 16:09:37 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-06.2@15d4852072c65b0451a2b52177a55b3cfdf81aad 🚀

---
+++ commonHelpers8.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const n=document.querySelector("#font-size-control"),o=document.querySelector("#text");n.addEventListener("input",r);function r(t){const{value:e}=t.currentTarget;o.style.fontSize=`${e}px`}
+//# sourceMappingURL=commonHelpers8.js.map
