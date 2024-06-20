34c22f5a2a10271001da96cda5a1131fa18a02e7
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-06-20 16:59:02 +0300

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginFormRef = document.querySelector('.login-form');
 
+loginFormRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const form = evt.target;
+  const emailValue = form.elements.email.value;
+  const passwordValue = form.elements.password.value;
+  const data = {};
+
+  if (emailValue === '' || passwordValue === '') {
+    const message = 'You must fill in all fields!!!';
+    alert(message);
+    return;
+  } else {
+    data.email = emailValue;
+    data.password = passwordValue;
+    console.log(data);
+  }
+  form.reset();
+}
