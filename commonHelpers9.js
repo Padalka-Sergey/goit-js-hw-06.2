cddeeb54dc3d1e3bce974611101e9c55d89f033e
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-06-20 13:59:20 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-06.2@34c22f5a2a10271001da96cda5a1131fa18a02e7 🚀

---
+++ commonHelpers9.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const a=document.querySelector(".login-form");a.addEventListener("submit",n);function n(t){t.preventDefault();const e=t.target,l=e.elements.email.value,o=e.elements.password.value,s={};if(l===""||o===""){alert("You must fill in all fields!!!");return}else s.email=l,s.password=o,console.log(s);e.reset()}
+//# sourceMappingURL=commonHelpers9.js.map
