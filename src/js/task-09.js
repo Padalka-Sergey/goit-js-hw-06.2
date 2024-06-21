488e6b1fa4b6a2590ae9142184164844a4689223
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-06-21 18:55:51 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const changeColorBtnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const textRef = document.querySelector('.color');
+
+changeColorBtnRef.addEventListener('click', clickBtnHandler);
+
+function clickBtnHandler() {
+  textRef.textContent = getRandomHexColor();
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
